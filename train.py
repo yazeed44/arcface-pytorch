4b0031b5ff9b227d27867ec18ac4a2e4b069404e
Yazeed Almuqwishi <yazeed.almuqwishi@gmail.com>
2021-05-12 15:28:22 +0300

    -Added training via arcface -Settings are now imported from settings.py -Improvements and optimizations

--- train.py
+++ train.py
@@ -1,19 +1,19 @@
+import os
 import time
 
-import os
 import numpy as np
 import torch
 import torchvision.datasets
+import torchvision.transforms as transforms
+from torch.nn import DataParallel
 from torch.optim.lr_scheduler import StepLR
 from torch.utils import data
 from torchvision.models import resnet50
-from torch.nn import DataParallel
 
 import settings
-from arc_margin_product import ArcMarginProduct
-from face_identity_dataset import FaceIdentityDataset
-import torchvision.transforms as transforms
+from arcface import Arcface
 
+from focal_loss import FocalLoss
 
 
 def save_model(model, save_path, name, iter_cnt):
@@ -38,50 +38,63 @@ image_transforms = transforms.Compose([
     torchvision.transforms.ConvertImageDtype(dtype=torch.float32)
 ])
 
-image_dataset = torchvision.datasets.ImageFolder("dataset/", transform=image_transforms)
+image_dataset = torchvision.datasets.ImageFolder(settings.image_dataset_dir, transform=image_transforms)
 train_loader = data.DataLoader(image_dataset, shuffle=True)
 
 num_of_classes = len(train_loader.dataset.class_to_idx)
-criterion = torch.nn.CrossEntropyLoss()
+criterion = torch.nn.CrossEntropyLoss() if settings.loss_function == "softmax" \
+    else FocalLoss(gamma=2)
+softmax = torch.nn.Softmax()
 model = resnet50(pretrained=False)
-metric_fc = torch.nn.Linear(in_features=2048, out_features=num_of_classes, bias=True)
+if settings.loss_function == "softmax":
+    model.fc = torch.nn.Linear(in_features=2048, out_features=num_of_classes)
+    optimizer = torch.optim.SGD([{'params': model.parameters()},
+                                 ],
+                                lr=1e-3, weight_decay=5e-4)
+    model.to(device)
+    model = DataParallel(model)
+else:
+    model.fc = torch.nn.Linear(in_features=2048, out_features=512)
+    metric_fc = Arcface(512, num_of_classes=num_of_classes)
+    optimizer = torch.optim.SGD([{'params': model.parameters()},
+                                 {'params': metric_fc.parameters()}
+                                 ],
+                                lr=1e-3,
+                                weight_decay=5e-4,
+                                momentum=0.9)
+    model.to(device)
+    model = DataParallel(model)
+    metric_fc.to(device)
+    metric_fc = DataParallel(metric_fc)
 
-model.fc = metric_fc
-model.to(device)
-model = DataParallel(model)
-# metric_fc.to(device)
-# metric_fc = DataParallel(metric_fc)
-optimizer = torch.optim.SGD([{'params': model.parameters()},
-                             # {'params': metric_fc.parameters()}
-                             ],
-                            lr=1e-1, weight_decay=5e-4)
 scheduler = StepLR(optimizer, step_size=10, gamma=0.1)
 correct_guesses = torch.tensor([], device=device, dtype=torch.bool)
-correct_guesses.to(device)
 
-for i in range(50):
+for i in range(settings.max_epoch):
 
     correct_guesses = torch.tensor([], device=device, dtype=torch.bool)
     correct_guesses.to(device)
     model.train()
     for ii, (image_data, image_label_id) in enumerate(train_loader):
+        optimizer.zero_grad()
         image_data, image_label_id = image_data.to(device), image_label_id.to(device)
         feature = model(image_data)
-        correct_guesses = torch.cat([correct_guesses, feature.argmax() == image_label_id])
+        # output = feature
+
+        output = feature if settings.loss_function == "softmax" else softmax(metric_fc(feature, image_label_id))
+        correct_guesses = torch.cat([correct_guesses, output.argmax() == image_label_id])
         correct_guesses.to(device)
-        loss = criterion(feature, image_label_id)
-        optimizer.zero_grad()
+        loss = criterion(output, image_label_id)
         loss.backward()
         optimizer.step()
-        scheduler.step()
 
-    iters = i * len(train_loader) + ii
-    acc = correct_guesses.sum() / len(correct_guesses)
-    time_str = time.asctime(time.localtime(time.time()))
-    print('{} train epoch {} iter {}  iters/s loss {} acc {}'.format(time_str, i, ii, loss.item(), acc))
+        iters = i * len(train_loader) + ii
+        acc = correct_guesses.sum() / len(correct_guesses)
+        time_str = time.asctime(time.localtime(time.time()))
+        print('{} train epoch {} iter {}  iters/s loss {} acc {}'.format(time_str, i, ii, loss.item(), acc))
 
+    scheduler.step()
     model.eval()
-    # acc = lfw_test(model, img_paths, identity_list, opt.lfw_test_list, opt.test_batch_size)
-    # if opt.display:
-    #     visualizer.display_current_results(iters, acc, name='test_acc')
-    save_model(model, "models/", "resnet50-crossentropy-", i)
+
+    save_model(model, settings.models_dir,
+               f"resnet50-{settings.loss_function}", i)
